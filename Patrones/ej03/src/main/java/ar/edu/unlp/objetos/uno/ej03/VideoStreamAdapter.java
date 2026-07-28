ec44b808e45f0e0ff27c1cd26f3ae6eeebc2231f
Lucas López <lopezlucas730@gmail.com>
2026-07-28 17:36:57 -0300

    Add constructor to VideoStreamAdapter

--- Patrones/ej03/src/main/java/ar/edu/unlp/objetos/uno/ej03/VideoStreamAdapter.java
+++ Patrones/ej03/src/main/java/ar/edu/unlp/objetos/uno/ej03/VideoStreamAdapter.java
@@ -3,7 +3,11 @@ package ar.edu.unlp.objetos.uno.ej03;
 public class VideoStreamAdapter extends Media {
 
 	private VideoStream videoStream;
-	
+
+	public VideoStream(VideoStream videoStream) {
+		this.videoStream = videoStream;
+	}
+		
 	public void play() {
 		this.videoStream.reproduce();
 	}
